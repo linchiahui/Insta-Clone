58e09fee75200381100ad81696fd02982505cf10
linchiahui <lin.chiahu@husky.neu.edu>
2020-03-06 21:18:29 -0800

    version 1

--- Insta/views.py
+++ Insta/views.py
@@ -1,5 +1,41 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+from Insta.models import Post
+# from django.contrib.auth.forms import UserCreationForm
+from Insta.forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HelloWorld(TemplateView):
     template_name = "test.html"
+
+class PostView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+    login_url = 'login'
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+    login_url = 'login'
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
+    login_url = 'login'
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
