4dd7d185fca8812d5fee2dea00acec3dbe621db6
linchiahui <lin.chiahu@husky.neu.edu>
2020-03-11 10:21:33 -0700

    Instagram version 1.0 finish

--- Insta/urls.py
+++ Insta/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from Insta.views import HelloWorld, PostView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from Insta.views import HelloWorld, PostView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserProfile, addLike, addComment, toggleFollow
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='helloworld'),
-    path('posts/', PostView.as_view(), name='posts'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
-    path('post/new', PostCreateView.as_view(), name='make_post'),
-    path('post/update/<int:pk>', PostUpdateView.as_view(), name='post_update'),
-    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
+    path('helloworld', HelloWorld.as_view(), name='helloworld'),
+    path('', PostView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
+    path('like', addLike, name='addLike'),
+    path('comment', addComment, name='addComment'),
+    path('togglefollow', toggleFollow, name='togglefollow'),
+    path('user_profile/<int:pk>/', UserProfile.as_view(), name='user_profile'),
 ]
