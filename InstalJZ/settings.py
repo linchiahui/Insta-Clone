8658c99c83eb81ced28bb5e337df1705618c73a7
linchiahui <lin.chiahu@husky.neu.edu>
2020-07-29 22:49:49 -0700

    prep for deploy

--- InstalJZ/settings.py
+++ InstalJZ/settings.py
@@ -131,6 +131,6 @@ LOGOUT_REDIRECT_URL = 'posts'
 
 AUTH_USER_MODEL = 'Insta.InstaUser'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
